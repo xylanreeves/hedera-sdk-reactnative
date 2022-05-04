210c684d10d6f74da0b350adbea5acc6213e274a
xylan <xylannerd@gmail.com>
2022-05-04 23:58:01 +0530

    1st commito 🪶

---
+++ App.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import { MY_ACCOUNT_ID, MY_PRIVATE_KEY } from '@env'
+
+import {
+  Client,
+  PrivateKey,
+  AccountCreateTransaction,
+  AccountBalanceQuery,
+  Hbar,
+  AccountId,
+} from '@hashgraph/sdk'
+
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  const [mAccount, setmAccount] = useState()
+  const [client, setClient] = useState<Client>()
+
+  useEffect(() => {
+    initHederaClient()
+  }, [])
+
+  async function initHederaClient() {
+    //Grab your Hedera testnet account ID and private key from your .env file
+    const myAccountId = MY_ACCOUNT_ID
+    const myPrivateKey = MY_PRIVATE_KEY
+
+    try {
+      // Create our connection to the Hedera network
+      // The Hedera JS SDK makes this really easy!
+      const _client = Client.forTestnet()
+      _client.setOperator(myAccountId, myPrivateKey)
+
+      setClient(_client)
+
+      //Create new keys
+      const newAccountPrivateKey = await PrivateKey.generateED25519()
+      const newAccountPublicKey = newAccountPrivateKey.publicKey
+
+      //Create a new account with 1,000 tinybar starting balance
+      const newAccount = await new AccountCreateTransaction()
+        .setKey(newAccountPublicKey)
+        .setInitialBalance(Hbar.fromTinybars(1000))
+        .execute(_client)
+
+        // Get the new account ID
+const getReceipt = await newAccount.getReceipt(_client);
+const newAccountId = getReceipt.accountId;
+setmAccount(newAccountId)
+
+//Log the account ID
+console.log("The new account ID is: " + newAccountId);
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Text>You new Account Id:</Text>
+      {mAccount && <Text>{mAccount.toString()}</Text>}
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
