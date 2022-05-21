b3dfc752906a3a4e7701cdedcc3d55183f751693
xylan <xylannerd@gmail.com>
2022-05-21 19:28:27 +0530

    mnemonic UI looking good

--- src/components/mnemonicScreen/mnemonicInput.tsx
+++ src/components/mnemonicScreen/mnemonicInput.tsx
@@ -1,89 +1,86 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
-
-
 export const initialValues = {
-    mnemonic1: '',
-    mnemonic2: '',
-    mnemonic3: '',
-    mnemonic4: '',
-    mnemonic5: '',
-    mnemonic6: '',
-    mnemonic7: '',
-    mnemonic8: '',
-    mnemonic9: '',
-    mnemonic10: '',
-    mnemonic11: '',
-    mnemonic12: '',
-    mnemonic13: '',
-    mnemonic14: '',
-    mnemonic15: '',
-    mnemonic16: '',
-    mnemonic17: '',
-    mnemonic18: '',
-    mnemonic19: '',
-    mnemonic20: '',
-    mnemonic21: '',
-    mnemonic22: '',
-    mnemonic23: '',
-    mnemonic24: '',
-  }
-  
-
+  mnemonic1: '',
+  mnemonic2: '',
+  mnemonic3: '',
+  mnemonic4: '',
+  mnemonic5: '',
+  mnemonic6: '',
+  mnemonic7: '',
+  mnemonic8: '',
+  mnemonic9: '',
+  mnemonic10: '',
+  mnemonic11: '',
+  mnemonic12: '',
+  mnemonic13: '',
+  mnemonic14: '',
+  mnemonic15: '',
+  mnemonic16: '',
+  mnemonic17: '',
+  mnemonic18: '',
+  mnemonic19: '',
+  mnemonic20: '',
+  mnemonic21: '',
+  mnemonic22: '',
+  mnemonic23: '',
+  mnemonic24: '',
+}
 
 const MnemonicInput = ({ mnemonicId, _onChangeText, _onBlur, _value }) => {
-    return (
-      <>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-            backgroundColor: 'green',
-            width: '150', 
-            
-          }}
-        >
-
-          <View
-            style={{
-              backgroundColor: 'rgb(31, 41, 55)',
-              borderRadius: 100,
-            }}
-          >
-
-            <Text style={{ color: 'rgb(156,163,175)', padding: 4 }}>
-              {mnemonicId}
-            </Text>
-
-          </View>
-
-          <TextInput
-            style={{
-              backgroundColor: 'rgb(31, 41, 55)',
-              color: 'rgb(156, 163, 175)',
-              padding: 8,
-              borderWidth: 1,
-              borderRadius: 8,
-              borderColor: 'rgba(107,114,128, 0.6)',
-              width: 120,
-              marginLeft: 8,
-            }}
-            placeholderTextColor="grey"
-            onChangeText={_onChangeText}
-            onBlur={_onBlur}
-            value={_value}
-          />
-
+  return (
+    <>
+      <View style={styles.mainContainer}>
+        <View style={styles.mnemonicIdContainer}>
+          <Text style={styles.mnemonicId}>{mnemonicId}</Text>
         </View>
-      </>
-    )
-  }
+
+        <TextInput
+          style={styles.inputBox}
+          placeholderTextColor="grey"
+          onChangeText={_onChangeText}
+          onBlur={_onBlur}
+          value={_value}
+        />
+      </View>
+    </>
+  )
+}
 
 export default MnemonicInput
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // justifyContent: 'flex-end',
+    // backgroundColor: 'green',
+    marginVertical: 4,
 
-    
+  },
+  inputBox: {
+    backgroundColor: 'rgb(31, 41, 55)',
+    color: 'rgb(156, 163, 175)',
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: 'rgba(107,114,128, 0.6)',
+    width: 150,
+    marginLeft: 8,
+  },
+  mnemonicIdContainer: {
+   backgroundColor: 'rgb(31, 41, 55)',
+    borderRadius: 100,
+    width: 32,
+    height: 32,
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+  mnemonicId: {
+    color: 'rgb(156,163,175)',
+    // backgroundColor: 'rgb(31, 41, 55)',
+    // borderRadius: 100,
+    padding: 4,
+  },
 })
--- src/screens/MnemonicScreen.tsx
+++ src/screens/MnemonicScreen.tsx
@@ -8,11 +8,11 @@ import {
   View,
   Text,
   TouchableOpacity,
-} from 'react-native'
-import {
   SafeAreaView,
-  withSafeAreaInsets,
-} from 'react-native-safe-area-context'
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native'
+
 import * as Yup from 'yup'
 import MnemonicInput, {
   initialValues,
@@ -41,209 +41,208 @@ const MnemonicScreen = () => (
         errors,
         isValid,
       }) => (
-        <View style={styles.container}>
-          <View style={styles.mnemonicInputContainer}>
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={1}
-                _onChangeText={handleChange('mnemonic1')}
-                _onBlur={handleBlur('mnemonic1')}
-                _value={values.mnemonic1}
-              />
+        <KeyboardAvoidingView>
+          <ScrollView>
+            <View style={styles.mnemonicInputContainer}>
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={1}
+                  _onChangeText={handleChange('mnemonic1')}
+                  _onBlur={handleBlur('mnemonic1')}
+                  _value={values.mnemonic1}
+                />
 
-              <MnemonicInput
-                mnemonicId={2}
-                _onChangeText={handleChange('mnemonic2')}
-                _onBlur={handleBlur('mnemonic2')}
-                _value={values.mnemonic2}
-              />
+                <MnemonicInput
+                  mnemonicId={2}
+                  _onChangeText={handleChange('mnemonic2')}
+                  _onBlur={handleBlur('mnemonic2')}
+                  _value={values.mnemonic2}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={3}
+                  _onChangeText={handleChange('mnemonic3')}
+                  _onBlur={handleBlur('mnemonic3')}
+                  _value={values.mnemonic3}
+                />
+                <MnemonicInput
+                  mnemonicId={4}
+                  _onChangeText={handleChange('mnemonic4')}
+                  _onBlur={handleBlur('mnemonic4')}
+                  _value={values.mnemonic4}
+                />
+              </View>
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={5}
+                  _onChangeText={handleChange('mnemonic5')}
+                  _onBlur={handleBlur('mnemonic5')}
+                  _value={values.mnemonic5}
+                />
+
+                <MnemonicInput
+                  mnemonicId={6}
+                  _onChangeText={handleChange('mnemonic6')}
+                  _onBlur={handleBlur('mnemonic6')}
+                  _value={values.mnemonic6}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={7}
+                  _onChangeText={handleChange('mnemonic7')}
+                  _onBlur={handleBlur('mnemonic7')}
+                  _value={values.mnemonic7}
+                />
+
+                <MnemonicInput
+                  mnemonicId={8}
+                  _onChangeText={handleChange('mnemonic8')}
+                  _onBlur={handleBlur('mnemonic8')}
+                  _value={values.mnemonic8}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={9}
+                  _onChangeText={handleChange('mnemonic9')}
+                  _onBlur={handleBlur('mnemonic9')}
+                  _value={values.mnemonic9}
+                />
+
+                <MnemonicInput
+                  mnemonicId={10}
+                  _onChangeText={handleChange('mnemonic10')}
+                  _onBlur={handleBlur('mnemonic10')}
+                  _value={values.mnemonic10}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={11}
+                  _onChangeText={handleChange('mnemonic11')}
+                  _onBlur={handleBlur('mnemonic11')}
+                  _value={values.mnemonic11}
+                />
+
+                <MnemonicInput
+                  mnemonicId={12}
+                  _onChangeText={handleChange('mnemonic12')}
+                  _onBlur={handleBlur('mnemonic12')}
+                  _value={values.mnemonic12}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={13}
+                  _onChangeText={handleChange('mnemonic13')}
+                  _onBlur={handleBlur('mnemonic13')}
+                  _value={values.mnemonic13}
+                />
+
+                <MnemonicInput
+                  mnemonicId={14}
+                  _onChangeText={handleChange('mnemonic14')}
+                  _onBlur={handleBlur('mnemonic14')}
+                  _value={values.mnemonic14}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={15}
+                  _onChangeText={handleChange('mnemonic15')}
+                  _onBlur={handleBlur('mnemonic15')}
+                  _value={values.mnemonic15}
+                />
+
+                <MnemonicInput
+                  mnemonicId={16}
+                  _onChangeText={handleChange('mnemonic16')}
+                  _onBlur={handleBlur('mnemonic16')}
+                  _value={values.mnemonic16}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={17}
+                  _onChangeText={handleChange('mnemonic17')}
+                  _onBlur={handleBlur('mnemonic17')}
+                  _value={values.mnemonic17}
+                />
+
+                <MnemonicInput
+                  mnemonicId={18}
+                  _onChangeText={handleChange('mnemonic18')}
+                  _onBlur={handleBlur('mnemonic18')}
+                  _value={values.mnemonic18}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={19}
+                  _onChangeText={handleChange('mnemonic19')}
+                  _onBlur={handleBlur('mnemonic19')}
+                  _value={values.mnemonic19}
+                />
+
+                <MnemonicInput
+                  mnemonicId={20}
+                  _onChangeText={handleChange('mnemonic20')}
+                  _onBlur={handleBlur('mnemonic20')}
+                  _value={values.mnemonic20}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={21}
+                  _onChangeText={handleChange('mnemonic21')}
+                  _onBlur={handleBlur('mnemonic21')}
+                  _value={values.mnemonic21}
+                />
+
+                <MnemonicInput
+                  mnemonicId={22}
+                  _onChangeText={handleChange('mnemonic22')}
+                  _onBlur={handleBlur('mnemonic22')}
+                  _value={values.mnemonic22}
+                />
+              </View>
+
+              <View style={styles.inputRows}>
+                <MnemonicInput
+                  mnemonicId={23}
+                  _onChangeText={handleChange('mnemonic23')}
+                  _onBlur={handleBlur('mnemonic23')}
+                  _value={values.mnemonic23}
+                />
+
+                <MnemonicInput
+                  mnemonicId={24}
+                  _onChangeText={handleChange('mnemonic24')}
+                  _onBlur={handleBlur('mnemonic24')}
+                  _value={values.mnemonic24}
+                />
+              </View>
             </View>
 
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={3}
-                _onChangeText={handleChange('mnemonic3')}
-                _onBlur={handleBlur('mnemonic3')}
-                _value={values.mnemonic3}
-              />
-              <MnemonicInput
-                mnemonicId={4}
-                _onChangeText={handleChange('mnemonic4')}
-                _onBlur={handleBlur('mnemonic4')}
-                _value={values.mnemonic4}
-              />
-            </View>
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={5}
-                _onChangeText={handleChange('mnemonic5')}
-                _onBlur={handleBlur('mnemonic5')}
-                _value={values.mnemonic5}
-              />
-
-              <MnemonicInput
-                mnemonicId={6}
-                _onChangeText={handleChange('mnemonic6')}
-                _onBlur={handleBlur('mnemonic6')}
-                _value={values.mnemonic6}
-              />
-            </View>
-
-            {/* /////////////////// */}
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={7}
-                _onChangeText={handleChange('mnemonic7')}
-                _onBlur={handleBlur('mnemonic7')}
-                _value={values.mnemonic7}
-              />
-
-              <MnemonicInput
-                mnemonicId={8}
-                _onChangeText={handleChange('mnemonic8')}
-                _onBlur={handleBlur('mnemonic8')}
-                _value={values.mnemonic8}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={9}
-                _onChangeText={handleChange('mnemonic9')}
-                _onBlur={handleBlur('mnemonic9')}
-                _value={values.mnemonic9}
-              />
-
-              <MnemonicInput
-                mnemonicId={10}
-                _onChangeText={handleChange('mnemonic10')}
-                _onBlur={handleBlur('mnemonic10')}
-                _value={values.mnemonic10}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={11}
-                _onChangeText={handleChange('mnemonic11')}
-                _onBlur={handleBlur('mnemonic11')}
-                _value={values.mnemonic11}
-              />
-
-              <MnemonicInput
-                mnemonicId={12}
-                _onChangeText={handleChange('mnemonic12')}
-                _onBlur={handleBlur('mnemonic12')}
-                _value={values.mnemonic12}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={13}
-                _onChangeText={handleChange('mnemonic13')}
-                _onBlur={handleBlur('mnemonic13')}
-                _value={values.mnemonic13}
-              />
-
-              <MnemonicInput
-                mnemonicId={14}
-                _onChangeText={handleChange('mnemonic14')}
-                _onBlur={handleBlur('mnemonic14')}
-                _value={values.mnemonic14}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={15}
-                _onChangeText={handleChange('mnemonic15')}
-                _onBlur={handleBlur('mnemonic15')}
-                _value={values.mnemonic15}
-              />
-
-              <MnemonicInput
-                mnemonicId={16}
-                _onChangeText={handleChange('mnemonic16')}
-                _onBlur={handleBlur('mnemonic16')}
-                _value={values.mnemonic16}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={17}
-                _onChangeText={handleChange('mnemonic17')}
-                _onBlur={handleBlur('mnemonic17')}
-                _value={values.mnemonic17}
-              />
-
-              <MnemonicInput
-                mnemonicId={18}
-                _onChangeText={handleChange('mnemonic18')}
-                _onBlur={handleBlur('mnemonic18')}
-                _value={values.mnemonic18}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={19}
-                _onChangeText={handleChange('mnemonic19')}
-                _onBlur={handleBlur('mnemonic19')}
-                _value={values.mnemonic19}
-              />
-
-              <MnemonicInput
-                mnemonicId={20}
-                _onChangeText={handleChange('mnemonic20')}
-                _onBlur={handleBlur('mnemonic20')}
-                _value={values.mnemonic20}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={21}
-                _onChangeText={handleChange('mnemonic21')}
-                _onBlur={handleBlur('mnemonic21')}
-                _value={values.mnemonic21}
-              />
-
-              <MnemonicInput
-                mnemonicId={22}
-                _onChangeText={handleChange('mnemonic22')}
-                _onBlur={handleBlur('mnemonic22')}
-                _value={values.mnemonic22}
-              />
-            </View>
-
-            <View style={styles.inputRows}>
-              <MnemonicInput
-                mnemonicId={23}
-                _onChangeText={handleChange('mnemonic23')}
-                _onBlur={handleBlur('mnemonic23')}
-                _value={values.mnemonic23}
-              />
-
-              <MnemonicInput
-                mnemonicId={24}
-                _onChangeText={handleChange('mnemonic24')}
-                _onBlur={handleBlur('mnemonic24')}
-                _value={values.mnemonic24}
-              />
-            </View>
-
-          </View>
-
-          <TouchableOpacity
-            style={styles.submitButton}
-            onPress={() => handleSubmit}
-          >
-            <Text>Submit</Text>
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity
+              style={styles.submitButton}
+              onPress={() => handleSubmit}
+            >
+              <Text>Submit</Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </KeyboardAvoidingView>
       )}
     </Formik>
   </SafeAreaView>
@@ -255,19 +254,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between',
-    padding: 16,
+    // justifyContent: 'space-between',
+    // padding: 16,
     // backgroundColor: 'rgb(31,41,55)',
     backgroundColor: 'rgb(17,24,39)',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
   mnemonicInputContainer: {
-    flex: 1,
+    // flex: 1,
+    marginTop: 64,
+    marginLeft: 13,
+    marginRight: 20,
     // backgroundColor: 'orange',
   },
   submitButton: {
     marginTop: 32,
+    marginHorizontal: 32,
+    marginBottom: 16,
     borderRadius: 16,
     padding: 16,
     backgroundColor: 'rgb(107,114,128)',
@@ -276,6 +280,7 @@ const styles = StyleSheet.create({
   },
   inputRows: {
     flexDirection: 'row',
+    // alignItems: 'center',
     justifyContent: 'space-between',
     // backgroundColor: 'yellow',
   },
